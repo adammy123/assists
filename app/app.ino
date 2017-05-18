b01219e501313ae2be0285692c71761c0d55cb8c
adammy123 <adam.sajc@gmail.com>
2017-05-18 15:08:12 -0500

    updated app.ino

--- app/app.ino
+++ app/app.ino
@@ -8,6 +8,23 @@ String AP_NameString = "Soccer Controls"; //Wifi name
 const char WiFiAPPSK[] = "soccerskilz";   //Password
 WiFiServer server(80);
 
+////////////////////////
+// System Definitions //
+////////////////////////
+const int sessionNumber = 1;
+const int totalBalls = 2;
+const int intervalTime = 3;   //seconds
+const float goTime = 0.5;     //seconds
+
+// Arrays to hold session results
+int target[totalBalls] = {0};
+int hit[totalBalls] = {0};
+float timeTaken[totalBalls] = {0.0};
+
+// Overall results
+int totalScore = 0;
+float totalTimeTaken = 0.0;
+
 String html_string;
 
 void setupWiFi(){
@@ -36,7 +53,7 @@ String home_(String html_string){
   html_string += "<body><p>Welcome to ASSISTS.<br>Brought to you by DSGN 384.<br><br>\r\n";
   html_string += "Instructions:<br>\r\n";
   html_string += "&nbsp;1. Set up goals 20 feet from center<br>\r\n";
-  html_string += "&nbsp;2. Have teammate ready with 10 balls<br>\r\n";
+  html_string += "&nbsp;2. Have teammate ready with " + String(totalBalls) + " balls<br>\r\n";
   html_string += "&nbsp;3. xxx<br><br>Press start when ready!</p>\r\n";
 
   // Button to redirect to the session page
@@ -52,21 +69,24 @@ String session(String html_string){
 
   // Javascript functions
   html_string += "<script>\r\n";
-  html_string += "var s=3;\r\n";
-  html_string += "var totalBalls=10;\r\n";
+  html_string += "var totalBalls=" + String(totalBalls) + ";\r\n";
   html_string += "var currentBall=1;\r\n";
+  html_string += "var intervalTime = " + String(intervalTime) + ";\r\n";
+  html_string += "var goTime = " + String(goTime) + ";\r\n";
   
   html_string += "function add(){\r\n";
-  html_string += "  s-=0.1;\r\n";
-  html_string += "  if(s < -0.1){\r\n";
+  html_string += "  intervalTime -= 0.1;\r\n";
+  html_string += "  if(intervalTime < -0.1){\r\n";
   html_string += "    document.getElementById('time').style.display = 'none';\r\n";
   html_string += "    document.getElementById('go').style.display = 'block';}\r\n";
   html_string += "  else{\r\n";
   html_string += "    document.getElementById('go').style.display = 'none';\r\n";
-  html_string += "    document.getElementById('time').innerHTML = s.toFixed(1);} \r\n";
-  html_string += "  if(s < -0.5){\r\n";
-  html_string += "    s=3; currentBall +=1;\r\n";
-  html_string += "    if (currentBall > totalBalls){clearTimeout(t);}\r\n";
+  html_string += "    document.getElementById('time').innerHTML = intervalTime.toFixed(1);} \r\n";
+  html_string += "  if(intervalTime < -goTime){\r\n";
+  html_string += "    intervalTime = 3; currentBall +=1;\r\n";
+  html_string += "    if (currentBall > totalBalls){\r\n";
+  html_string += "      document.getElementById('button').style.display ='block';\r\n";
+  html_string += "      clearTimeout(t);}\r\n";
   html_string += "    document.getElementById('time').style.display = 'block';\r\n";
   html_string += "    document.getElementById('numBalls').innerHTML = 'Ball: ' + currentBall;}\r\n";
   html_string += "  stopwatch();\r\n";
@@ -85,7 +105,7 @@ String session(String html_string){
   html_string += "<p id=\"go\" style=\"font-size: 200pt; background-color: rgb(0, 255, 0); text-align: center; display: none;\">GO!</p>\r\n";
 
   // Button redirects to results page
-  html_string += "<button type=\"button\" onclick=\"location.href = '/results';\" style=\"height:100px;width:500px; margin: 0 auto;\">See results</button>\r\n";
+  html_string += "<button id=\"button\" type=\"button\" onclick=\"location.href = '/results';\" style=\"height:100px;width:500px; margin: 0 auto; display: none;\">See results</button>\r\n";
   html_string += "</body></html>\r\n";
   
   return html_string;
@@ -110,17 +130,35 @@ String results(String html_string){
 
   // Overall statistics
   html_string += "<body onload=\"startTime()\">\r\n";
-  html_string += "<div id=\"session\">Session: X</div>\r\n";
+  html_string += "<div id=\"session\">Session: " + String(sessionNumber) + "</div>\r\n";
   html_string += "<div id=\"date\"></div>\r\n";
   html_string += "<div id=\"time\"></div>\r\n";
-  html_string += "<div id=\"score\">Score: </div>\r\n";
-  html_string += "<div id=\"timeTaken\">Time Taken: </div>\r\n";
+  html_string += "<div id=\"score\">Score: ";
+  for(int i = 0; i < totalBalls; i++){
+    if(hit[i] == 1){
+      totalScore += 1; 
+    }
+  }
+  html_string += String(totalScore) + "</div>\r\n";
+  html_string += "<div id=\"timeTaken\">Time Taken: ";
+  for(int i = 0; i < totalBalls; i++){
+    totalTimeTaken += timeTaken[i]; 
+  }
+  html_string += String(totalTimeTaken) + "</div>\r\n";
 
   // Draw table for statistics on individual targets
   html_string += "<table>\r\n";
   html_string += "<tr><th>Ball #</th><th>Target</th><th>Hit?</th><th>Time</th></tr>\r\n";
-  for(int i = 1; i < 11; i++){
-    html_string += "<tr><td>" + String(i) + "</td><td>X</td><td>HIT</td><td>X</td></tr>\r\n";
+  for(int i = 1; i <= totalBalls; i++){
+    html_string += "<tr><td>" + String(i) + "</td>";                    //Ball number
+    html_string += "<td>" + String(target[i-1]) + "</td>";              //Target
+    if (hit[i-1] == 1){                                                 //HIT
+      html_string += "<td>HIT</td>";
+    }
+    else{                                                               //MISS
+      html_string += "<td>MISS</td>";
+    }
+    html_string += "<td>" + String(timeTaken[i-1]) + "</td></tr>\r\n";  //Time taken
   }
   html_string += "</table><br>\r\n";
 
